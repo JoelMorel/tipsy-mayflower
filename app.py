a3a43568d1c5f83ee59dedaf8e427786ea1693ca
JoelMorel <Joel@JoelMorel.com>
2020-08-25 23:00:53 -0400

    removed navbar. added dynamic search of businesses based on location via yelp API

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
-# from LivePopularTimes import livepopulartimes
 import get_popularity
 import mysql.connector
 app = Flask(__name__)
@@ -20,28 +19,13 @@ else:
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # db = SQLAlchemy(app)
-db = mysql.connector.connect(
-    host='arfo8ynm6olw6vpn.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	',
-    user='ledia1lyvd5ncahd',
-    password='n8x650vva6u3z1mo',
-    port='3306',
-    database='m4q3e12uaejpc9ld',
-)
-
-
-# class location(db.Model):
-#     __tablename__ = 'location'
-#     id = db.column(db.Integer, primary_key=True)
-#     name = db.column(db.String(200), unique=True)
-#     address = db.column(db.String(200))
-#     current_popularity = db.column(db.Integer)
-#     rating = db.column(db.Integer)
-
-#     def __int__(self, name, address, current_popularity, rating):
-#         self.name = name
-#         self.address = address
-#         self.current_popularity = current_popularity
-#         self.rating = rating
+# db = mysql.connector.connect(
+#     host='arfo8ynm6olw6vpn.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	',
+#     user='ledia1lyvd5ncahd',
+#     password='n8x650vva6u3z1mo',
+#     port='3306',
+#     database='m4q3e12uaejpc9ld',
+# )
 
 
 @app.route('/')
@@ -53,8 +37,8 @@ def index():
 def submit():
     if request.method == 'POST':
         city = request.form['city']
-        # location = request.form['location']
-        if city == '':
+        location = request.form['location']
+        if city == '' and location == '':
             return render_template('index.html')
         pop_spots = get_popularity.checkCity(city)
         return render_template('results.html', city=city, spots=pop_spots)
--- get_popularity.py
+++ get_popularity.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 from LivePopularTimes import livepopulartimes
+import get_listOfLocations
 
 app = Flask(__name__)
 
@@ -9,43 +10,36 @@ proxy = 0
 
 formatted_address_NYC = ['(Mamajuana Cafe Queens), 33-15 56th St, Queens, NY 11377', '(The Delancy), 168 Delancey St, New York, NY 10002', '(Yeras), 86-09 Northern Blvd, Jackson Heights, NY 11372', '(The Ainsworth Midtown), 45 E 33rd St, New York, NY 10016',
                          '(Banc Cafe), 431 3rd Ave, New York, NY 10016', '(The Gem Saloon), 375 3rd Ave, New York, NY 10016', '(230 Fifth Rooftop Bar), 230 5th Ave, New York, NY 10001', '(Pier 17), 89 South St, New York, NY 10038', '(Frying Pan), 207 12th Ave, New York, NY 10001', '(GITANO NYC), 76 Varick St, New York, NY 10013']
-formatted_address_AC = ['(The Pool After Dark), 777 Harrahs Blvd, Atlantic City, NJ 08401', '(Premier Nightclub), 1 Borgata Way, Atlantic City, NJ 08401', '(HQ2 Nightclub | Beachclub), 500 Boardwalk, Atlantic City, NJ 08401', '(DAER Nightclub), 1000 Boardwalk, Atlantic City, NJ 08401',
-                        '(Anthem Lounge), 2801 Pacific Ave #303, Atlantic City, NJ 08401', '(Boogie Nights), 2831 Boardwalk, Atlantic City, NJ 08401', '(The Chelsea Beach Bar), 3001-3099, Boardwalk, Atlantic City, NJ 08401', '(Ballys Beach Bar), 1900 Boardwalk, Atlantic City, NJ 08401']
-formatted_address_RVC = ['(Kaseys Kitchen & Cocktails), 23 N Park Ave, Rockville Centre, NY 11570', '(The New Vibe Lounge), 60 N Park Ave, Rockville Centre, NY 11570',
-                         '(Croxleys Great American Ale House), 7 S Park Ave, Rockville Centre, NY 11570', '(Dark Horse Tavern), 12 S Park Ave, Rockville Centre, NY 11570']
+# formatted_address_AC = ['(The Pool After Dark), 777 Harrahs Blvd, Atlantic City, NJ 08401', '(Premier Nightclub), 1 Borgata Way, Atlantic City, NJ 08401', '(HQ2 Nightclub | Beachclub), 500 Boardwalk, Atlantic City, NJ 08401', '(DAER Nightclub), 1000 Boardwalk, Atlantic City, NJ 08401',
+#                         '(Anthem Lounge), 2801 Pacific Ave #303, Atlantic City, NJ 08401', '(Boogie Nights), 2831 Boardwalk, Atlantic City, NJ 08401', '(The Chelsea Beach Bar), 3001-3099, Boardwalk, Atlantic City, NJ 08401', '(Ballys Beach Bar), 1900 Boardwalk, Atlantic City, NJ 08401']
+# formatted_address_RVC = ['(Kaseys Kitchen & Cocktails), 23 N Park Ave, Rockville Centre, NY 11570', '(The New Vibe Lounge), 60 N Park Ave, Rockville Centre, NY 11570',
+#                          '(Croxleys Great American Ale House), 7 S Park Ave, Rockville Centre, NY 11570', '(Dark Horse Tavern), 12 S Park Ave, Rockville Centre, NY 11570']
 
 
 def checkCity(cityName):
     responseData = {}
 
-    if cityName == 'NYC':
-        for i in formatted_address_NYC:
-            response = livepopulartimes.get_populartimes_by_address(
-                i, proxy=proxy)
-            name = response['name']
-            address = response['address']
-            current_popularity = response['current_popularity']
-            rating = response['rating']
-            responseData[i] = dict(name=name, address=address,
-                                   current_popularity=current_popularity, rating=rating)
-        responseList = responseData.values()
-        return responseList
-
     if cityName == 'AC':
-        for i in formatted_address_AC:
-            response = livepopulartimes.get_populartimes_by_address(
-                i, proxy=proxy)
-            name = response['name']
-            address = response['address']
-            current_popularity = response['current_popularity']
-            rating = response['rating']
-            responseData[i] = dict(name=name, address=address,
-                                   current_popularity=current_popularity, rating=rating)
+        listOfLocations = get_listOfLocations.createList('Atlantic City')
+        for i in listOfLocations:
+            try:
+                response = livepopulartimes.get_populartimes_by_address(
+                    i, proxy=proxy)
+                name = response['name']
+                address = response['address']
+                current_popularity = response['current_popularity']
+                rating = response['rating']
+                responseData[i] = dict(name=name, address=address,
+                                       current_popularity=current_popularity, rating=rating)
+            except(IndexError, KeyError, TypeError):
+                print('ERROR in call: ' + i)
         responseList = responseData.values()
         return responseList
 
     if cityName == 'RVC':
-        for i in formatted_address_RVC:
+        listOfLocations = get_listOfLocations.createList('Rockville Centre')
+
+        for i in listOfLocations:
             response = livepopulartimes.get_populartimes_by_address(
                 i, proxy=proxy)
             name = response['name']
@@ -56,6 +50,28 @@ def checkCity(cityName):
                                    current_popularity=current_popularity, rating=rating)
         responseList = responseData.values()
         return responseList
+
+    responseList = getList(cityName)
+    return responseList
+
+
+def getList(cityName):
+    responseData = {}
+    listOfLocations = get_listOfLocations.createList(cityName)
+    for i in listOfLocations:
+        try:
+            response = livepopulartimes.get_populartimes_by_address(
+                i, proxy=proxy)
+            name = response['name']
+            address = response['address']
+            current_popularity = response['current_popularity']
+            rating = response['rating']
+            responseData[i] = dict(name=name, address=address,
+                                   current_popularity=current_popularity, rating=rating)
+        except(IndexError, KeyError, TypeError):
+            print('ERROR in call: ' + i)
+    responseList = responseData.values()
+    return responseList
 
 
 app.route('/results')
